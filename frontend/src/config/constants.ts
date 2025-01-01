223de43513e2936c039d908eb286dbe1cba968ad
Anusree <anilkumaranusree113@gmail.com>
2025-01-01 18:14:10 +0530

    fixed frontendurl in frontend

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -2,4 +2,4 @@
 export const backendUrl = "https://roc8-moonshot-assessment.onrender.com";
 // export const frontendUrl = "http://localhost:3000";
 export const frontendUrl =
-  "https://roc8-moonshot-assessment-frontend.vercel.app/";
+  "https://roc8-moonshot-assessment-frontend.vercel.app";
