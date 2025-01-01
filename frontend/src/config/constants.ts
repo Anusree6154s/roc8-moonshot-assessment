7f108bd76ef1061346b8f0b74f0e053dfdb85d16
Anusree <anilkumaranusree113@gmail.com>
2025-01-01 15:47:32 +0530

    added deployed frontend and backend

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,2 +1,5 @@
-export const backendUrl = 'http://localhost:8000'
-export const frontendUrl = 'http://localhost:3000'
+// export const backendUrl = 'http://localhost:8000'
+export const backendUrl = "https://roc8-moonshot-assessment.onrender.com";
+// export const frontendUrl = "http://localhost:3000";
+export const frontendUrl =
+  "https://roc8-moonshot-assessment-frontend.vercel.app/";
